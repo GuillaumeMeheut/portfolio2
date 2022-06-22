2440d16ca341387007da6ea0cc7f4872bbcc25e2
Guillaume Meheut <guillaume.meheutpro@gmail.com>
2022-06-22 14:20:43 +0200

    remove projects

--- utils/projects.ts
+++ utils/projects.ts
@@ -5,17 +5,4 @@ export type ProjectProps = {
   imgsUrl: string[]
 }
 
-export const data: ProjectProps[] = [
-  {
-    id: 1,
-    title: 'Obonto',
-    type: 'Prêt immobilier',
-    imgsUrl: ['/assets/obonto1.jpg', '/assets/obonto1.jpg'],
-  },
-  {
-    id: 2,
-    title: 'Scorpulting',
-    type: 'Agence',
-    imgsUrl: ['/assets/scorpulting1.jpg'],
-  },
-]
+export const data: ProjectProps[] = []
