9af66e015962b3fd77f588a6d5bcd393fea7762b
Guillaume <guillaume.meheutpro@gmail.com>
2025-04-30 19:54:27 +0200

    add hotjar script in app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import { appWithTranslation } from 'next-i18next'
 import { useEffect } from 'react'
 import { isMobile } from 'utils/isMobile'
 import style from 'config/index.module.scss'
+import Script from 'next/script'
 
 const PAGE_MAX_WIDTH = parseFloat(style.pageMaxWidth) * 10 + 80
 
@@ -30,6 +31,18 @@ function MyApp({ Component, pageProps, router }: AppProps) {
     <>
       <Transition Key={router.route} />
       <Component {...pageProps} />
+      <Script id="hotjar" strategy="afterInteractive">
+        {`
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:6384688,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+         `}
+      </Script>
     </>
   )
 }
